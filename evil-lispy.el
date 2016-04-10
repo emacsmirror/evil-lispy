836750c90bd900f504cfc46496963cf7ae5ef942
Mika Vilpas <mika.vilpas@gmail.com>
2016-04-10 15:03:51 +0300

    Fix gv (mark current symbol) binding not selecting strings
    It would select the current word inside the current string instead.

--- evil-lispy.el
+++ evil-lispy.el
@@ -109,7 +109,7 @@ the current form.  DIRECTION must be either 'left or 'right."
   "Enters `lispy-state' with the current symbol under point marked."
   (interactive)
   (evil-lispy-state)
-  (lispy-mark-symbol))
+  (lispy-mark))
 
 (defun evil-lispy-enter-visual-state ()
   "If we're in visual state, enter `lispy-state' with the current region
