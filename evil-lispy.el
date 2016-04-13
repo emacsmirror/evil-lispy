854d300cdd6fdf793dc164ae63cc04e966d919e3
Mika Vilpas <mika.vilpas@gmail.com>
2016-04-13 13:33:23 +0300

    Remove notice about custom delete commands having a toggle variable

--- evil-lispy.el
+++ evil-lispy.el
@@ -29,9 +29,7 @@
 ;; evil-lispy defines a minor mode and an additional Evil state for editing
 ;; Lisp code.  The goal is to encourage a workflow where you can hop between
 ;; Lispy State for making structured edits using Lispy bindings and the rest
-;; of the standard Evil states for general editing.  Where it makes sense,
-;; this package redefines a few Lispy bindings, which can be turned on or off
-;; by a variable.
+;; of the standard Evil states for general editing.
 
 ;;; Code:
 
