794b528e609467c01139fe2d96b4553c05df8dc7
Mika Vilpas <mika.vilpas@gmail.com>
2016-04-16 19:05:47 +0300

    Fix not loading evil-lispy before running tests

--- run-tests.sh
+++ run-tests.sh
@@ -5,6 +5,7 @@ TERM=dumb SHELL=sh cask exec emacs \
     -batch \
     -f package-initialize \
     -l buttercup \
+    -l *.el \
     -l "tests/setup.el" \
     -f buttercup-run-discover \
     "tests/"
