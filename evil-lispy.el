726035e51dcf1bbbe60a6deb72d40d417260db5b
Mika Vilpas <mika.vilpas@gmail.com>
2016-04-30 08:00:17 +0300

    Add explicit package version numbers in dependencies

--- evil-lispy.el
+++ evil-lispy.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/sp3ctum/evil-lispy
 ;; Version: 0.0.1
 ;; Keywords: lisp
-;; Package-Requires: (lispy evil)
+;; Package-Requires: ((lispy "0.26.0") (evil "1.2.12"))
 
 
 ;; This file is not part of GNU Emacs
