ff3e38c289237d53a85b168872c40a6d77c0f010
Mika Vilpas <mika.vilpas@gmail.com>
2016-05-21 09:14:38 +0300

    Add a separate navigation cheat sheet (help)

--- evil-lispy-help.el
+++ evil-lispy-help.el
@@ -1,23 +1,60 @@
 (require 'lispy)
 (require 'hydra)
 
-(defhydra evil-lispy/hydra ()
+(defhydra evil-lispy/hydra-navigation ()
   "
-evil-lispy basics
+Navigation cheat sheet
+----------------------
 
-^Navigate^                 ^Refactor^               ^Evaluate^
-^^^^^^^^-----------------------------------------------------------------------
-_h_: backward & up         _t_: teleport            _e_: eval
-_j_: next                  _d_: drag                _E_: eval and insert
-_k_: previous              _c_: clone
-_l_: forward & up          _w_: move up
-_o_: other side            _s_: move down
-_i_: inside                _r_: raise
-_q_: jump to ()            _O_: to oneliner
-_-_: go to subword         _M_: to multiline
+                       Starting from _|_, navigate to:
+_h_: backward & up    |
+_j_: next             |  _h_(->
+_k_: previous         |       _k_(+ 2 3)
+_l_: forward & up     |       _|_(+ _i_(+ 2 1))_o_
+_o_: other side       |       _j_(* 8))_l_
+_i_: inside           |
+
+
+Miscellaneous
+-------------
+_q_: jump to ()
+_-_: go to subword
 
 
 "
+  ("h" special-lispy-left)
+  ("j" special-lispy-down)
+  ("k" special-lispy-up)
+  ("l" special-lispy-right)
+  ("o" special-lispy-different)
+  ("|" identity :color teal)
+  ("i" special-lispy-flow)
+  ("q" special-lispy-ace-paren)
+  ("-" special-lispy-ace-subword))
+
+(evil-lispy/hydra-navigation/body)
+
+(defhydra evil-lispy/hydra ()
+  "
+evil-lispy cheat sheet
+----------------------
+
+_N_ Navigation cheat sheet
+
+^Refactor^               ^Evaluate^
+----------------------------------------------------
+_t_  teleport            _e_  eval
+_d_  drag                _E_  eval and insert
+_c_  clone
+_w_  move up
+_s_  move down
+_r_  raise
+_O_  to oneliner
+_M_  to multiline
+
+
+"
+  ("N" evil-lispy/hydra-navigation/body :color teal)
   ("h" special-lispy-left)
   ("j" special-lispy-down)
   ("k" special-lispy-up)
