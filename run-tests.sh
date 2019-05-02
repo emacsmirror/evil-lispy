1d933bd382306c3eca0b0410ab5cc635968d77bf
Mika Vilpas <mika.vilpas@gmail.com>
2019-05-02 10:23:53 +0300

    blöö

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,9 @@
 set -x # show executed commands
 set -e # exit on error
 
+# This directory gets created and stored on the developer's machine and it can't
+# be deleted by anyone else than root
+rm -rf .cask/
 cask
 
 TERM=dumb SHELL=sh cask exec emacs \
